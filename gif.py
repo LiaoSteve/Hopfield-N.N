1364b64cf4806e40934759737b0e25b8d5c351c5
steve-Liao <steveliao1688@gmail.com>
2019-12-04 19:17:17 +0800

    add new

---
+++ gif.py
@@ -0,0 +1,19 @@
+#------------  Use the .png of the folder to create the .gif
+import os
+import imageio
+
+dirName1 = 'result/noise/'
+dirName2 = 'result/unknown/'
+dirName3 = 'result/both/'
+Name = []
+dirs = os.listdir(dirName3) #--- change dirName 1,2,3
+frames = []
+gif_name1 = 'noise.gif'
+gif_name2 = 'unknown.gif'
+gif_name3 = 'bo th .gif'
+
+for file in dirs:
+    frames.append(imageio.imread(dirName3 + file)) #-- change dirName 1,2,3
+    imageio.mimsave(gif_name3, frames, 'GIF', duration = 1.5) #-- change gif_name 1,2,3
+    print (file) 
+
